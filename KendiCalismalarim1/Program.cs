83fd01182827166c356dfb116428db035b110088
arifkayhan44 <kayhanarif44@gmail.com>
2021-01-26 18:57:00 +0300

    Add project files.

---
+++ KendiCalismalarim1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KendiCalismalarim1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ariffff!!!");
+        }
+    }
+}
